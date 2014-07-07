26cbbafe66ca3c513bcaf671e128898786e9e231
MaggieTHU <linym012@163.com>
2014-07-07 09:53:34 +0800

    hw3-网络学堂大改造-返回顶部.js

---
+++ hw3/to-Top/改造-返回顶部.js
@@ -0,0 +1,3 @@
+$(' <link rel="stylesheet" type="text/css" href="https://MaggieTHU.github.io/hw3/to-Top/to-top.css"/>').appendTo('body');
+$('<img src="https://MaggieTHU.github.io/hw3/to-Top/to-top.png" id="toTop" width="50px" />').appendTo('body');
+$('<script style="text/javascript" src="https://MaggieTHU.github.io/hw3/to-Top/to-top.js"/>').appendTo('body');
