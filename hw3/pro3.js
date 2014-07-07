c64a39a73ca31ff4f845142cf23edc4abc9288ac
MaggieTHU <linym012@163.com>
2014-07-07 20:54:27 +0800

    margin-left放入<p>中，生效

--- hw3/pro3.js
+++ hw3/pro3.js
@@ -1,8 +1,9 @@
 x = $('.lh30:visible');
-t = $('<td style = "margin-left:10px"/>');
-t[0].innerText = '1';
-x.prepend(t[0])
-for(i = 0; i<x.length; i++){x[i].firstChild.innerText = (i+1).toString();}
+t = $('<td/>');
+z = $('<p style = "margin-left:10px"/>');
+z[0].innerText ='1';
+z.appendTo(t);
+x.prepend(t[0]);
+for(i = 0; i<x.length; i++){x[i].firstChild.firstChild.innerText = (i+1).toString();}
 y = $('td.student');
 for(i = 0; i<y.length; i++){y[i].childNodes[0].innerText = '1073741824'};
-
